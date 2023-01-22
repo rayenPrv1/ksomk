ffa1db8e4ff19f3f8ecac5b3dd25b634fd6a271c
kqadora <56276815+KadOraYT@users.noreply.github.com>
2023-01-22 09:52:37 -0930

    qsfsq

---
+++ config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+module.exports = {
+  ToKeN: process.env.ToKeN,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  DATABASE: process.env.DATABASE,
+  CLIENT_ID: "963400731736567809",
+  API_URL: "https://Twix.kqadorardp1.repl.co",
+  GUILD_ID: "1057063218255765504",
+  GUILD_invite: "https://discord.gg/hly",
+  ROLE_ID: "1062131727625109584",
+  PREFIX: "1",
+  DEVS: ["769590521328828457"]
+         
+}
